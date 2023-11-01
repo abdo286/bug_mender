f2444bec0744f35e96c3cd20a026c2062f0581fb
Abdulrahman Mohammed <123945345+abdo286@users.noreply.github.com>
2023-11-01 19:05:11 +0200

    Update supabaseClient.js

--- src/libs/supabaseClient.js
+++ src/libs/supabaseClient.js
@@ -1,13 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabase = createClient(
-//   import.meta.env.VITE_SUPABASEURL,
-//   import.meta.env.VITE_SUPABASEANONKEY
-// );
-
 const supabase = createClient(
-  "https://gffeowlkccsjegwpcvlb.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmZmVvd2xrY2NzamVnd3BjdmxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5OTEzNTcsImV4cCI6MjAwOTU2NzM1N30.dpMYZ6w3KtDmTxw4T1OJeZnnoXs9qzs__QKzIjRVFwI"
+  import.meta.env.VITE_SUPABASEURL,
+  import.meta.env.VITE_SUPABASEANONKEY
 );
 
 export { supabase };
